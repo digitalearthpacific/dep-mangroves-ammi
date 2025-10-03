1831f11ca9682e5989d176c81f33330c0a44fbe8
Sachindra Singh <sachindras@spc.int>
2025-10-03 14:56:31 +1200

    Elevation Masking Test.

--- src/util.py
+++ src/util.py
@@ -1,26 +1,53 @@
-import cv2
-import numpy as np
+#import cv2
+#import numpy as np
+from xarray import DataArray, Dataset
+from odc.stac import load
+from pystac_client import Client
 
+def apply_mask(
+    ds: Dataset,
+    mask: DataArray,
+    ds_to_mask: Dataset | None = None,
+    return_mask: bool = False,
+) -> Dataset:
+    """Applies a mask to a dataset"""
+    to_mask = ds if ds_to_mask is None else ds_to_mask
+    masked = to_mask.where(mask)
+
+    if return_mask:
+        return masked, mask
+    else:
+        return masked
+
+
+def mask_elevation(
+    ds: Dataset,
+    ds_to_mask: Dataset | None = None,
+    threshold: float = 10,
+    return_mask: bool = False,
+) -> Dataset:
+    """
+    Mask elevation. Returns 1 for high areas, 0 for low
+    """
+    e84_catalog = "https://earth-search.aws.element84.com/v1/"
+    e84_client = Client.open(e84_catalog)
+    collection = "cop-dem-glo-30"
+
+    items = e84_client.search(
+        collections=[collection], bbox=list(ds.odc.geobox.geographic_extent.boundingbox)
+    ).item_collection()
+
+    # Using geobox means it will load the elevation data the same shape as the other data
+    elevation = load(items, measurements=["data"], geobox=ds.odc.geobox).squeeze()
+
+    # True where data is above elevation
+    mask = elevation.data > threshold
+
+    return apply_mask(ds, mask, ds_to_mask, return_mask)
+
+"""
 def filter_mask(closing_kernel_size, opening_kernel_size, mask):
-    """
-    Given a mask, apply morphological filters (closing followed by opening) 
-    to filter out unwanted pixels.
-
-    Parameters:
-    -----------
-       closing_kernel_size : Int
-                             Size of the closing kernel in pixels.
-       opening_kernel_size : Int
-                             Size of the opening kernel in pixels.
-        mask : Array[int]
-               A binary mask.
     
-    Returns:
-    --------
-        mask_closed_opened : Array[int]
-               A morphologically filtered binary mask.
-    """
-
     ## Closing filter: Remove empty pixels within mask
     # Create a kernel element which is closing_kernel_size^2 in size
     closing_kernel_element = (closing_kernel_size, closing_kernel_size)
@@ -45,3 +72,5 @@ def filter_mask(closing_kernel_size, opening_kernel_size, mask):
     mask_closed_opened[mask_closed_opened == 0] = np.nan
 
     return mask_closed_opened
+    
+"""
